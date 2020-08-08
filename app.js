76c398f605c43e09b022cdea0377a5834a7ba8a7
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-08 09:53:55 +0100

    fix scaling and spred bar charts

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const DUMMY_DATA = [
 ]
 
 const xScale = d3.scaleBand()
+    .domain(DUMMY_DATA.map(dataPoint => dataPoint.region)) // let the scale funciton to know how many point of data it has
     .rangeRound([0, 250]) //tells d3 how much space is avialable
     .padding(0.1);
 
 const yScale = d3.scaleLinear()
     .domain([0, 15]) // our way o f letting d3 min and max values that it has to map around
+    .range([200, 0]) 
 
 const container = d3.select('svg')
     .style('border', '1px solid red')
@@ -24,6 +26,7 @@ const bars = container
     .enter()
     .append('rect')
     .classed('bar', true)
-    .attr('width', 50)
-    .attr('height', data => data.value * 15)
-    .text(data => `This is ${data.region}`)
+    .attr('width', xScale.bandwidth())
+    .attr('height', data => 200 - yScale(data.value))
+    .attr('x', data => xScale(data.region))
+    .attr('y', data => yScale(data.value))
