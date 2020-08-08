eb5332a141e5ca63916f0e6fc1f46cf40cce6950
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-08 09:42:04 +0100

    add yscale fundtion to map value to y axes

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const DUMMY_DATA = [
     {id: 'd4', value: 6, region: "Germany"}
 ]
 
+const xScale = d3.scaleBand()
+    .rangeRound([0, 250]) //tells d3 how much space is avialable
+    .padding(0.1);
+
+const yScale = d3.scaleLinear()
+    .domain([0, 15]) // our way o f letting d3 min and max values that it has to map around
 
 const container = d3.select('svg')
     .style('border', '1px solid red')
@@ -18,6 +24,6 @@ const bars = container
     .enter()
     .append('rect')
     .classed('bar', true)
-    .attr('width', '50px')
+    .attr('width', 50)
     .attr('height', data => data.value * 15)
     .text(data => `This is ${data.region}`)
