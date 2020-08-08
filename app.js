f145c33de33679fa31e82e0d7258147437f4f6c2
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-08 10:48:18 +0100

    add more data

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const DUMMY_DATA = [
     {id: 'd1', value: 10, region: "USA"},
     {id: 'd2  ', value: 11, region: "India"},
     {id: 'd3', value: 12, region: "China"},
-    {id: 'd4', value: 6, region: "Germany"}
+    {id: 'd4', value: 6, region: "Germany"},
+    {id: 'd5', value: 2, region: "Russia"},
 ]
 
 const xScale = d3.scaleBand()
