2d1b9ea5963843125e4e4bb97c4b7a3a71f701d0
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-08 11:12:36 +0100

    initiliase d3

---
+++ src/components/circle.js
@@ -0,0 +1,14 @@
+import React, {useRef, useEffect} from 'react'
+
+const Circle = () => {
+    const ref  = useRef()
+
+    return (
+           <svg>
+
+            </svg>
+    
+    )
+}
+
+export default Circle
