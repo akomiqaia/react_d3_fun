7a5223a0805da0f473b66d2e03e740490d33cceb
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-09 20:47:46 +0100

    add styling

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import dataSet from './utils/MOCK_DATA'
 
 function App() {
   const [data, setData] = useState(dataSet)
+  const width = 600
+  let dimensions = {
+    width: width,
+    height: width * 0.5,
+    margin: {
+      top: 35,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    },
+  }
   useEffect(() => {
     setData(dataSet)
     console.log(data)
   }, [data])
 
   return (
-    <div className="App">
-            <Chart width={500} height={500} dataSet={data} />
+        <div className="App">
+            <Chart width={width} height={500} dataSet={data} dimensions={dimensions} />
         </div>
   );
 }
