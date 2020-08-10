e5889853d44378422c08abc95fbfb464f0f4ef67
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-10 11:16:54 +0100

    change zoom funciton for zoomnig liner chart instead of bar chart

--- src/components/chart.js
+++ src/components/chart.js
@@ -19,7 +19,6 @@ const Chart = ({ width, height, dataSet }) => {
       .scaleUtc()
       .domain(d3.extent(dataSet, d => new Date(d.usageDate)))
       .range([margin.left, width - margin.right])
-      // .padding(0.1
 
     let y = d3
       .scaleLinear()
@@ -28,84 +27,74 @@ const Chart = ({ width, height, dataSet }) => {
       .range([height - margin.bottom, margin.top]);
 
     let xAxis = (g) =>
-      g.attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3
-          .axisBottom(x)
-          .tickSizeOuter(0)
-      );
-
-    let yAxis = (g) =>
       g
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y))
-        .call((g) => g.select(".domain").remove())
-        .call((g) =>
-          g
-            .append("text")
-            .attr("x", -margin.left)
-            .attr("y", 10)
-            .attr("text-anchor", "start")
-            .attr("font-weight", "bold")
-            .text("percentage")
+      .attr("class", "x-axis")
+      .attr("clip-path", "url(#clip)")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3
+        .axisBottom(x)
+        .tickSizeOuter(0)
         );
-
-     
-        function zoom(svg) {
-          const extent = [[margin.left, margin.top], [width - margin.right, height - margin.top]];
         
-          svg.call(d3.zoom()
-              .scaleExtent([1, 8])
-              .translateExtent(extent)
-              .extent(extent)
-              .on("zoom", zoomed));
+    let yAxis = (g) =>
+    g
+    .attr("class", "y-axis")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y))
         
-          function zoomed() {
-            x.range([margin.left, width - margin.right].map(d => d3.event.transform.applyX(d)));
-            svg.selectAll(".bars rect").attr("x", d => x(d.usageDate)).attr("width", x.bandwidth());
-            svg.selectAll(".x-axis").call(xAxis);
-          }
-        }
+      
+        
 
     const svg = d3
       .select(ref.current)
       .attr("viewBox", [0, 0, width, height])
       .call(zoom)
 
-        // ucoment this to get bar graph
-    // svg
-    //   .append("g")
-    //     .attr("class", "bars")
-    //     .attr("fill", "steelblue")
-    //   .selectAll("rect")
-    //   .data(dataSet)
-    //   .join("rect")
-    //     .attr("x", (d) => x(d.usageDate))
-    //     .attr("y", (d) => y(d.percentage))
-    //     .attr("height", (d) => y(0) - y(d.percentage))
-    //     .attr("width", x.bandwidth());
+    svg.append("defs").append("clipPath")
+      .attr("id", "clip")
+      .append("rect")
+      .attr("x", margin.left)
+      .attr("width", width - margin.right)
+      .attr("height", height);
    
-             //uncomment this to get line graph
     const line = d3.line()
         .defined(d => !isNaN(d.percentage))
         .x(d => x(d.usageDate))
         .y(d => y(d.percentage))
+
     svg.append("path")
         .datum(dataSet)
+        .attr("class", "path")
         .attr("fill", "none")
+        .attr("clip-path", "url(#clip)")
         .attr("stroke", "steelblue")
         .attr("stroke-width", 1.5)
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
         .attr("d", line);
 
     svg.append("g")
-      .attr("class", "x-axis")
       .call(xAxis);
 
     svg.append("g")
-      .attr("class", "y-axis")
       .call(yAxis);
-  
+
+    function zoom(svg) {
+        const extent = [
+          [margin.left, margin.top], 
+          [width - margin.right, height - margin.top]
+        ];
+      
+        svg.call(d3.zoom()
+            .scaleExtent([1, 3])
+            .translateExtent(extent)
+            .extent(extent)
+            .on("zoom", zoomed));
+      
+        function zoomed() {
+          x.range([margin.left, width - margin.right].map(d => d3.event.transform.applyX(d)));
+          svg.select("path").attr("d", line)
+          svg.select(".x-axis").call(d3.axisBottom(x).tickSizeOuter(0));
+        }
+      }
   }, [dataSet, height, timeData, width]);
 
   return (
