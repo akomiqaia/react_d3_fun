b8503853a3d0980306c7426d8292db167313683d
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-10 12:18:22 +0100

    add zoom funcitonality on the line

--- src/components/chart.js
+++ src/components/chart.js
@@ -5,7 +5,7 @@ import * as d3 from "d3";
 // import {drawChart, initChart} from '../utils/drawChart'
 
 const Chart = ({ width, height, dataSet }) => {
-  const timeData = dataSet.map((x) => x.usageDate);
+  const timeData = dataSet.map((x) => new Date(x.usageDate));
   console.log(timeData.map(x => new Date(x)))
   dataSet = dataSet.map(
     ({ usageDate, percentage }) => ({ usageDate, percentage }),
@@ -48,8 +48,13 @@ const Chart = ({ width, height, dataSet }) => {
     const svg = d3
       .select(ref.current)
       .attr("viewBox", [0, 0, width, height])
-      .call(zoom)
+      
 
+    const line = d3.line()
+        .defined(d => !isNaN(d.percentage))
+        .x(d => x(new Date(d.usageDate)))
+        .y(d => y(d.percentage))
+        
     svg.append("defs").append("clipPath")
       .attr("id", "clip")
       .append("rect")
@@ -57,10 +62,6 @@ const Chart = ({ width, height, dataSet }) => {
       .attr("width", width - margin.right)
       .attr("height", height);
    
-    const line = d3.line()
-        .defined(d => !isNaN(d.percentage))
-        .x(d => x(d.usageDate))
-        .y(d => y(d.percentage))
 
     svg.append("path")
         .datum(dataSet)
@@ -68,6 +69,8 @@ const Chart = ({ width, height, dataSet }) => {
         .attr("fill", "none")
         .attr("clip-path", "url(#clip)")
         .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
         .attr("stroke-width", 1.5)
         .attr("d", line);
 
@@ -76,7 +79,8 @@ const Chart = ({ width, height, dataSet }) => {
 
     svg.append("g")
       .call(yAxis);
-
+    svg.call(zoom)
+    svg.call(hover)
     function zoom(svg) {
         const extent = [
           [margin.left, margin.top], 
@@ -94,6 +98,66 @@ const Chart = ({ width, height, dataSet }) => {
           svg.select("path").attr("d", line)
           svg.select(".x-axis").call(d3.axisBottom(x).tickSizeOuter(0));
         }
+      }
+
+      function hover() {
+      let bisect = d3.bisector(d => d.date).left,
+			format = d3.format("+.0%"),
+			dateFormat = d3.timeFormat("%Y-%b-%d")
+
+		var focus = svg.append("g")
+			.attr("class", "focus")
+			.style("display", "none");
+
+		focus.append("line")
+			.attr("stroke", "#666")
+			.attr("stroke-width", 1)
+			.attr("y1", -height + margin.top)
+			.attr("y2", -margin.bottom);
+
+		focus.append("circle")
+			.attr("class", "circle")
+			.attr("r", 5)
+			.attr("dy", 5)
+			.attr("stroke", "steelblue")
+			.attr("fill", "#fff");
+
+		focus.append("text")
+			.attr("text-anchor", "middle")
+			.attr("dy", ".35em");
+
+		let overlay = svg.append("rect")
+			.attr("class", "overlay")
+			.attr("x", margin.left)
+			.attr("y", margin.top)
+			.attr("width", width - margin.right - margin.left - 1)
+			.attr("height", height - margin.bottom - margin.top)
+			.on("mouseover", () => focus.style("display", null))
+			.on("mouseout", () => focus.style("display", "none"))
+      .on("mousemove", mousemove);
+      
+      function mousemove() {
+
+        var x0 = x.invert(d3.mouse(this)[0]);
+  
+        var i = bisect(data, x0, 1),
+          d0 = data[i - 1],
+          d1 = data[i],
+          d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+  
+        focus.select("line")
+          .attr("transform", 
+            "translate(" + x(d.date) + "," + height + ")");
+  
+        focus.selectAll(".circle")
+          .attr("transform", 
+            "translate(" + x(d.date) + "," + y(d.value) + ")");
+  
+        focus.select("text")
+          .attr("transform", 
+            "translate(" + x(d.date) + "," + (height + margin.bottom) + ")")
+          .text(dateFormat(d.date));
+      }
       }
   }, [dataSet, height, timeData, width]);
 
--- src/utils/MOCK_DATA.js
+++ src/utils/MOCK_DATA.js
@@ -1,21 +1,17 @@
 const dataSet = [
-  {usageDate: "2007-04-25", percentage: 98.84},
-  {usageDate: "2007-04-29", percentage: 99.8},
-  {usageDate: "2007-04-26", percentage: 99.92},
-  {usageDate: "2007-05-01", percentage: 99.47},
-  {usageDate: "2007-05-02", percentage: 100.39},
-  {usageDate: "2007-05-03", percentage: 100.4},
-  {usageDate: "2007-05-04", percentage: 100.81},
-  {usageDate: "2007-05-07", percentage: 103.92},
-  {usageDate: "2007-05-08", percentage: 105.06},
-  {usageDate: "2007-05-09", percentage: 106.88},
-  {usageDate: "2007-05-09", percentage: 107.34},
-  {usageDate: "2007-05-10", percentage: 108.74},
-  {usageDate: "2007-05-13", percentage: 109.36},
-  {usageDate: "2007-05-14", percentage: 107.52},
-  {usageDate: "2007-05-15", percentage: 107.34},
-  {usageDate: "2007-05-16", percentage: 109.44},
-  {usageDate: "2007-05-17", percentage: 110.02}
+  {"usageDate": "2011-05-02", "percentage": 100.39},
+  {"usageDate": "2012-05-03", "percentage": 100.4},
+  {"usageDate": "2014-05-04", "percentage": 100.81},
+  {"usageDate": "2017-05-07", "percentage": 103.92},
+  {"usageDate": "2018-05-08", "percentage": 105.06},
+  {"usageDate": "2019-05-09", "percentage": 106.88},
+  {"usageDate": "2019-05-09", "percentage": 107.34},
+  {"usageDate": "2019-05-10", "percentage": 108.74},
+  {"usageDate": "2019-05-13", "percentage": 109.36},
+  {"usageDate": "2019-05-14", "percentage": 107.52},
+  {"usageDate": "2019-05-15", "percentage": 107.34},
+  {"usageDate": "2019-05-16", "percentage": 109.44},
+  {"usageDate": "2020-05-17", "percentage": 110.02}
 ]
 
 export  default dataSet
